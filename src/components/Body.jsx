9147e2709008a6db99788acd92f7a26384a8bc95
BryanHidalgo00 <bryanmj2021@gmail.com>
2022-06-09 01:54:46 -0500

    dfsfg

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -7,9 +7,11 @@ export const Body = () => {
       <div className="hola">Hola ñiña JohannaUwu♡</div>
       <div className="content">
         <p className="pe">Toca el botón que quieras</p>
-        <button className="btn" onClick={()=>setContador(contador+"♡ sos lo mejor  | ")}> Tocame</button>
-        <button className="btn" onClick={()=>setContador(contador+"♡ te quiero ver | ")}> Tocame</button>
-        <p>{contador}</p>
+        <button className="btn" onClick={()=>setContador("| ♡ Sos lo mejor  | ")}> 1</button>
+        <button className="btn" onClick={()=>setContador("| ♡ Te quiero ver | ")}> 2</button>
+        <a href="https://www.youtube.com/watch?v=HJllxBL1w5g"> <button className="btn btn-1">3</button> </a>
+        
+ 
       </div>
     </>
   );
